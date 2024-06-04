d44a94c2d4bb38964bf88398221130e8a1b788ed
Michal Tomaszewski <groobytt@gmail.com>
2024-06-04 22:41:20 +0200

    code cleanup

--- backend/src/main/java/com/mtomaszewski/zadania/service/CurrencyRateService.java
+++ backend/src/main/java/com/mtomaszewski/zadania/service/CurrencyRateService.java
@@ -2,7 +2,7 @@ package com.mtomaszewski.zadania.service;
 
 import com.mtomaszewski.zadania.dto.Currency.CurrencyRequestDto;
 import com.mtomaszewski.zadania.dto.Currency.CurrencyResponseDto;
-import com.mtomaszewski.zadania.model.Currency;
+
 
 public interface CurrencyRateService {
     CurrencyResponseDto getCurrentValueCommand(CurrencyRequestDto currencyRequest);
